26101e9737b83d43eacc0e066e727dbd2641014c
kytoaq <kytoaq@gmail.com>
2021-04-15 03:56:17 -0500

    doha: Fixup overrides with Clang
    Follow error:
    clang++: error: no such file or directory: 'ro.build.fingerprint=ro.system.build.fingerprint'
    clang++: error: no such file or directory: 'ro.build.version.incremental'
    clang++: error: no such file or directory: 'ro.product.device=ro.product.system.device'
    clang++: error: no such file or directory: 'ro.product.model=ro.product.system.model'
    clang++: error: no such file or directory: 'ro.product.name=ro.product.system.name'
    clang++: error: no such file or directory: 'ro.build.version.release'
    clang++: error: no such file or directory: 'ro.build.version.security_patch'
    
    Fix-up: Write all in one line
    
    Signed-off-by: kytoaq <kytoaq@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,14 +102,8 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
 
 # System props override
 TW_OVERRIDE_SYSTEM_PROPS := \
-    ro.build.product \
-    ro.build.fingerprint=ro.system.build.fingerprint \
-    ro.build.version.incremental \
-    ro.product.device=ro.product.system.device \
-    ro.product.model=ro.product.system.model \
-    ro.product.name=ro.product.system.name \
-    ro.build.version.release \
-    ro.build.version.security_patch
+    "ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name;ro.build.version.release;ro.build.version.security_patch"
+
 
 # Kernel
 BOARD_KERNEL_CMDLINE := \
